6e05936a44ee1134daef96113fe5757f3332e7c8
ribnil <ribnil>
2020-07-06 02:58:00 +0900

    Config管理クラス

--- src/plugin/Config/ConfigBase.php
+++ src/plugin/Config/ConfigBase.php
@@ -4,6 +4,41 @@ declare(strict_types=1);
 
 namespace plugin\Config;
 
+use plugin\Main;
+use pocketmine\utils\Config;
+
 class ConfigBase
 {
+	/** @var array */
+	private static $config = [];
+
+	static function init(Main $main): void {
+		self::register([
+			ConfigList::PLAYER,
+			ConfigList::SHOP,
+			ConfigList::CLUB,
+			ConfigList::JOB_COUNT
+		], $main->getDataFolder());
+	}
+
+	static function getFor(string $type): ?Config {
+		return self::$config[$type] ?? null;
+	}
+
+	private static function isExist(string $key): bool {
+		return isset(self::$config[$key]);
+	}
+
+	private static function register(array $keys, string $path): void {
+		foreach($keys as $key) {
+			self::registerFor($key, $path);
+		}
+	}
+
+	private static function registerFor(string $key, string $path): void {
+		if(self::isExist($key))
+			return;
+
+		self::$config[$key] = new Config($path.$key."yml", Config::YAML);
+	}
 }
---
+++ src/plugin/Config/ConfigList.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace plugin\Config;
+
+class ConfigList
+{
+	const PLAYER = "player";
+
+	const SHOP = "price";
+
+	const CLUB = "club";
+
+	const JOB_COUNT = "job_count";
+}
