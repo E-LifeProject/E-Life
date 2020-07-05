eac116c2870581c330ae0ff5477dc588849efdb3
ribnil <ribnil>
2020-07-06 02:40:25 +0900

    ItenListの実装

---
+++ src/plugin/Item/Original/ItemList.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+class ItemList
+{
+	const MENU_BOOK_NAME = "§l§aメインメニュー";
+	const MENU_BOOK_LORE = ["本サーバーのメインメニューを開くことができるアイテム\n持っていたら無敵という訳ではない"];
+}
---
+++ src/plugin/Item/Original/MenuBook.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace plugin\Item\Original;
+
+use ItemList;
+use OriginItem;
+use pocketmine\item\enchantment\Enchantment;
+use pocketmine\item\enchantment\EnchantmentInstance;
+use pocketmine\item\Item;
+use pocketmine\item\ItemIds;
+use pocketmine\Player;
+
+class MenuBook extends OriginItem
+{
+	public function __construct() {
+		parent::__construct(ItemIds::BOOK);
+
+		$this->addEnchantment(new EnchantmentInstance(Enchantment::getEnchantment(26), 5));
+		$this->setCustomName(ItemList::MENU_BOOK_NAME);
+		$this->setLore(ItemList::MENU_BOOK_LORE);
+	}
+
+	public function onUse(Player $player): void {
+		//TODO; 処理の実装
+	}
+}
