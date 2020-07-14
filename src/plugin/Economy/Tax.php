09710a7d005148a553450650813a8d93b747d9be
FonSoutan <soju.komatsu@gmail.com>
2020-07-14 10:19:29 +0900

    税金の種類に応じて計算する

--- src/plugin/Economy/Tax.php
+++ src/plugin/Economy/Tax.php
@@ -2,13 +2,12 @@
 
 namespace plugin\Economy;
 
-#Basic
-
-
-
-
-
 class Tax{
+
+    public const CONSUMPTION_TAX = "consumption";
+    public const CORPORATE_TAX = "corporate";
+    public const GIFT_TAX = "gift";
+    public const INCOME_TAX = "income";
 
     private $money;
     
@@ -16,10 +15,22 @@ class Tax{
         $this->money = $money;
     }
 
+    /**
+     * 税金を計算して返す
+     * 税金を含めた合計金額を返すわけではないので注意
+     */
+
     public function TaxCalculate(int $mode): int{
-        //場合わけすればいいん？
-        $totalMoney = $this->money * $consumptionTax;
-        return $totalMoney;
+        if(TaxList::CONSUMPTION_TAX === $mode){
+            $tax = $this->money * $consumptionTax;
+        }elseif(TaxList::CORPORATE_TAX === $mode){
+            $tax = $this->money * $corporateTax;
+        }elseif(TaxList::GIFT_TAX === $mode){
+            $tax = $this->money * $giftTax;
+        }elseif(TaxList::INCOME_TAX === $mode){
+            $tax = $this->money * $incomeTax;
+        }
+        return $tax;
     }
 }
 
