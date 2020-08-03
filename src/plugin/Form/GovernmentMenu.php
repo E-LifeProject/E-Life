a11ccc9aff27df495e16e423790012c62916c102
FonSoutan <soju.komatsu@gmail.com>
2020-08-03 19:45:46 +0900

    空白の削除など見やすくした

--- src/plugin/Form/GovernmentMenu.php
+++ src/plugin/Form/GovernmentMenu.php
@@ -7,28 +7,35 @@ use pocketmine\Player;
 use pocketmine\form\Form;
 
 #E-Life
+use plugin\Form\Government\PurchaseForm;
+use plugin\Form\Government\GovernmentDepositBalance;
+use plugin\Form\Government\GovernmentOfficial;
 
-
-class governmentMenu implements Form{
+class GovernmentMenu implements Form{
     public function handleResponse(Player $player,$data):void{
         if($data === null){
             return;
         }
         switch($data){
+
+            //政府ブロックorアイテム買取
             case 0:
-                $player->sendForm();
+                $player->sendForm(new PurchaseForm());
             break;
 
+            //政府の預金残高を照会（国庫)
             case 1:
-                $player->sendForm();
+                $player->sendForm(new GovernmentDepositBalance());
             break;
 
+            //権限者の確認
             case 2:
-                $player->sendForm();
+                $player->sendForm(new GovernmentOfficial());
             break;
 
+            //市町村別の財政を管理したり、政府関係者が市町村の管理をする
             case 3:
-                $player->sendForm();
+                $player->sendForm(new AdministrativeManagement());
             break;
         }
     }
@@ -40,10 +47,10 @@ class governmentMenu implements Form{
             'content'=>'実行したい項目を選択してください',
             'buttons'=>[
                 [
-                    'text'=>'ブロック買取'
+                    'text'=>'資源買取フォーム'
                 ],
                 [
-                    'text'=>'国庫金の確認'
+                    'text'=>'政府預金残高照会'
                 ],
                 [
                     'text'=>'政府関係者リスト'
