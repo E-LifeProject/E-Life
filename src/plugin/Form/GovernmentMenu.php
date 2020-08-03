bd225755b75ec5f11852e68c82bb4d18c90de7d3
FonSoutan <soju.komatsu@gmail.com>
2020-08-03 16:46:56 +0900

    Formの要素を追加

--- src/plugin/Form/GovernmentMenu.php
+++ src/plugin/Form/GovernmentMenu.php
@@ -6,10 +6,31 @@ namespace plugin\Form;
 use pocketmine\Player;
 use pocketmine\form\Form;
 
+#E-Life
+
 
 class governmentMenu implements Form{
     public function handleResponse(Player $player,$data):void{
+        if($data === null){
+            return;
+        }
+        switch($data){
+            case 0:
+                $player->sendForm();
+            break;
 
+            case 1:
+                $player->sendForm();
+            break;
+
+            case 2:
+                $player->sendForm();
+            break;
+
+            case 3:
+                $player->sendForm();
+            break;
+        }
     }
 
     public function jsonSerialize(){
@@ -22,13 +43,13 @@ class governmentMenu implements Form{
                     'text'=>'ブロック買取'
                 ],
                 [
-                    'text'=>'国庫残高確認'
+                    'text'=>'国庫金の確認'
                 ],
                 [
-                    'text'=>'市町村残高確認'
+                    'text'=>'政府関係者リスト'
                 ],
                 [
-                    'text'=>'行政管理(操作権限がある方のみ)'
+                    'text'=>'行政管理(関係者のみ)'
                 ]
             ]
         ];
