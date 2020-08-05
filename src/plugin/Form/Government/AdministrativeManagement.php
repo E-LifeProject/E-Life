3d0ac51b1dc5f8e10d846313d8b78f346d69b5d4
FonSoutan <soju.komatsu@gmail.com>
2020-08-05 21:45:24 +0900

    買取品目の変更を政府管理者ができるように

---
+++ src/plugin/Form/Government/AdministrativeManagement.php
@@ -0,0 +1,131 @@
+<?php 
+
+namespace plugin\Form\Government;
+
+#Basic
+use pocketmine\Player;
+use pocketmine\form\Form;
+
+#E-Life
+use plugin\Form\Government\Purchase;
+use plugin\Form\Government\GovernmentDepositBalance;
+use plugin\Form\Government\GovernmentOfficial;
+use plugin\Form\Government\AdministrativeManagement;
+use plugin\Form\Government\GovernmentManagement;
+use plugin\Form\Government\ItemChange;
+use plugin\Config\ConfigBase;
+use plugin\Config\ConfigList;
+
+class AdministrativeManagement implements Form{
+    public function handleResponse(Player $player,$data):void{
+        if($data === null){
+            return;
+        }
+        switch($data){
+
+            //政府管理
+            case 0:
+                if($player->isOp()){
+                    $player->sendForm(new GovernmentManagement());
+                }else{
+                    $player->sendMessage("§a[個人通知] §7あなたは政府役人ではありません");
+                }
+            break;
+
+        }
+    }
+
+    public function jsonSerialize(){
+        return[
+            'type'=>'form',
+            'title'=>'政府系フォーム',
+            'content'=>'実行したい項目を選択してください',
+            'buttons'=>[
+                [
+                    'text'=>'政府管理'
+                ]
+            ]
+        ];
+    }
+}
+
+class GovernmentManagement implements Form{
+    public function handleResponse(Player $player,$data):void{
+        if($data === null){
+            return;
+        }
+        switch($data){
+
+            //政府買取品目変更
+            case 0:
+                $player->sendForm(new ItemChange());
+            break;
+
+        }
+    }
+
+    public function jsonSerialize(){
+        return[
+            'type'=>'form',
+            'title'=>'政府系フォーム',
+            'content'=>'実行したい項目を選択してください',
+            'buttons'=>[
+                [
+                    'text'=>'買取品目変更'
+                ]
+            ]
+        ];
+    }
+
+}
+
+
+class ItemChange implements Form{
+    public function handleResponse(Player $player,$data):void{
+        if($data === null){
+            return;
+        }
+        switch($data[0]){
+
+            //石
+            case 0:
+                $config = ConfigBase::getFor(ConfigList::PURCHASE);
+                $config->set("setItem","stone");
+                $config->save();
+            break;
+
+            case 1:
+                $config = ConfigBase::getFor(ConfigList::PURCHASE);
+                $config->set("setItem","oka");
+                $config->save();
+            break;
+
+            case 2:
+                $config = ConfigBase::getFor(ConfigList::PURCHASE);
+                $config->set("setItem","ironOre");
+                $config->save();
+            break;
+        }
+    }
+
+    public function jsonSerialize(){
+        return[
+            'type'=>'custom_form',
+            'title'=>'政府買取品目変更',
+            'content'=>[
+                [
+                    'type'=>'dropdown',
+                    'text'=>'買取品目を選択してください',
+                    'options'=>[
+                        '石',
+                        'オークの原木',
+                        '鉄鉱石'
+                    ],
+                    'default'=>2
+                ]
+            ]
+        ];
+    }
+
+}
+?>
