68c556dbe9d5009f253140bb629f3e39809d9fdb
FonSoutan <soju.komatsu@gmail.com>
2020-08-15 01:43:37 +0900

    口座が開設されているかの判定コードを追加

--- src/plugin/Form/BankMenu.php
+++ src/plugin/Form/BankMenu.php
@@ -7,7 +7,13 @@ use pocketmine\Player;
 use pocketmine\form\Form;
 
 #E-Life
+use plugin\Economy\Bank;
+use plugin\Form\Bank\WithdrawMoney;
+use plugin\Form\Bank\MoneyDeposit;
+use plugin\Form\Bank\BalanceInquiry;
+use plugin\Form\Bank\CashTransfer;
 use plugin\Form\Bank\accountOpening;
+
 
 class BankMenu implements Form{
 
@@ -16,25 +22,48 @@ class BankMenu implements Form{
             return;
         }
 
+
+        /**
+         * 銀行関連のFormを送る前に
+         * 口座があるか判定してから送信する
+         */
+
         switch($data){
             //お引き出し
             case 0:
-                $player->sendForm(new Purchase());
+                if(Bank::getInstance()->checkAccount($player->getName())){
+                    $player->sendForm(new WithdrawMoney($player->getName()));
+                }else{
+                    $player->sendMessage("§a[個人通知] §7口座が開設されておりません");
+                }
             break;
 
             //お預入れ
             case 1:
-                $player->sendForm(new GovernmentDepositBalance());
+                if(Bank::getInsatnce()->checkAccount($player->getName())){
+                    $player->sendForm(new MoneyDeposit());
+                }else{
+                    $player->sendMessage("§a[個人通知] §7口座が開設されておりません");
+                }
             break;
 
             //残高照会
             case 2:
-                $player->sendForm(new LocalFinance());
+                if(Bank::getInsatnce()->checkAccount($player->getName())){
+                    $player->sendForm(new BalanceInquiry());
+                }else{
+                    $player->sendMessage("§a[個人通知] §7口座が開設されておりません");
+                }
             break;
+
+            /**
+             * 所持金と口座両方から振り込みできるように
+             * するから銀行口座の判定はこの段階では必要ない
+             */
 
             //お振込
             case 3:
-                $player->sendForm(new GovernmentOfficial());
+                $player->sendForm(new CashTransfer());
             break;
 
             //口座開設
