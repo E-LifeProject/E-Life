72d648e7cc6dd5ae2ca003fd86085f8c0312eee4
FonSoutan <soju.komatsu@gmail.com>
2020-08-16 23:29:16 +0900

    ローンについての関数を追加

--- src/plugin/Economy/Bank.php
+++ src/plugin/Economy/Bank.php
@@ -38,7 +38,7 @@ class Bank{
 
     //預金残高
     public function getDepositBalance($name){
-        return $this->getConfig()->getNested($name.".DepositBalance");
+        return $this->getAccountConfig()->getNested($name.".DepositBalance");
     }
 
     //預金残高追加
@@ -46,7 +46,7 @@ class Bank{
         $depositBalance = $this->getDepositBalance($name);
         $depositBalance += $money;
         $this->setDepositBalance($name,$depositBalance);
-        $this->save();
+        $this->saveAccountConfig();
     }
 
     //預金残高出金
@@ -54,12 +54,46 @@ class Bank{
         $depositBalance = $this->getDepositBalance($name);
         $depositBalance -= $money;
         $this->setDepositBalance($name,$depositBalance);
-        $this->save();
+        $this->saveAccountConfig();
     }
 
     //銀行口座が開設されているか確認
     public function checkAccount($name){
-        if($this->getConfig()->exists($name)){
+        if($this->getAccountConfig()->exists($name)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    //ローンの残金を取得
+    public function getLoan($name){
+        return $this->getBankConfig()->getNested($name.".Loan");
+    }
+
+    //ローンを申請
+    public function applicationLoan($name,$money){
+        $this->getLoanConfig()->set($name,$money);
+        $this->saveLoanConfig();
+    }
+
+    //ローンを許可して追加
+    public function addLoan($name){
+        $this->getLoanConfig()->remove($name);
+        $this->saveLoanConfig();
+    }
+
+    //ローンを返済
+    public function repaymentLoan($name,$money){
+        $loan = $this->getAccountConfig()->getNested($name.".Loan");
+        $loan -= $money;
+        $this->getAccountConfig()->setNested($name.".Loan",$loan);
+        $this->saveAccountConfig();
+    }
+
+    //ローンがあるか確認
+    public function checkLoan($name){
+        if($this->getBankConfig()->getLoan($name) > 0){
             return true;
         }else{
             return false;
@@ -67,9 +101,6 @@ class Bank{
     }
 
     
-    /**
-     * 銀行の資本金や純資金のクラスを後ほど作る
-     */
 
     //口座開設
     public function accountOpening($name){
@@ -78,18 +109,70 @@ class Bank{
             ));
         $this->save();
     }
+
+    /**
+     * ローンの資金の出所は銀行の資金から出す
+     * もしローンの回収がきちんと出来てなければ銀行は新規ローンの追加が不可能
+     * の為、ローン返済されない場合は口座停止などのペナルティを追加するべき
+     */
+
+    //銀行資金を確認
+    public function getBankMoney(){
+        return $this->getBankConfig()->getBankConfig("money");
+    }
+
+    //銀行資金を追加
+    public function addBankMoney($money){
+        $now = $this->getBankMoney();
+        $now += $money;
+        $this->setBankMoney($now);
+        $this->getBankConfig()->saveBankConfig();
+    }
+
+    //銀行資金を減らす
+    public function reduceBankMoney($money){
+        $now = $this->getBankMoney();
+        $now += $money;
+        $this->setBankMoney($now);
+        $this->getBankMoney()->saveBankConfig();
+    }
+
+
+
+
+
     
+
+    private function setBankMoney($money){
+        $this->getBankConfig()->set("money",$money);
+    }
 
     //configに書き込む用
     private function setDepositBalance($name,$depositBalance){
         $this->getConfig()->setNested($name.".DepositBalance",$depositBalance);
     }
     
-    private function getConfig(){
+    private function getAccountConfig(){
         return ConfigBase::getFor(ConfigList::BANK_ACCOUNT);
     }
+
+    private function getBankCounfig(){
+        return ConfigBase::getFor(ConfigList::BANK);
+    }
+
+    private function getLoanConfig(){
+        return ConfigBase::getFor(ConfigList::Loan);
+    }
     
-    private function save(){
-        $this->getConfig()->save();
+    private function saveAccountConfig(){
+        $this->getAccountConfig()->save();
+    }
+
+    private function saveBankConfig(){
+        $this->getBankConfig()->save();
+    }
+
+    private function saveLoanConfig(){
+        $this->getLoanConfig()->save();
     }
 }
