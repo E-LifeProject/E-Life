cdf9144ed45e1c2b421b15b9985cc91916f770b6
FonSoutan <soju.komatsu@gmail.com>
2020-08-20 21:06:12 +0900

    受け取りフォームを追加

--- src/plugin/Form/GovernmentMenu.php
+++ src/plugin/Form/GovernmentMenu.php
@@ -7,7 +7,10 @@ use pocketmine\Player;
 use pocketmine\form\Form;
 
 #E-Life
+use plugin\Economy\MoneyListener;
+use plugin\Economy\Government\GovernmentMoney;
 use plugin\Form\Government\Purchase;
+use plugin\Form\Government\CashReceipt;
 use plugin\Form\Government\GovernmentDepositBalance;
 use plugin\Form\Government\GovernmentOfficial;
 use plugin\Form\Government\AdministrativeManagement;
@@ -24,24 +27,45 @@ class GovernmentMenu implements Form{
             case 0:
                 $player->sendForm(new Purchase());
             break;
+            
+            //保管金受け取り
+            case 1:
+                $instance = new MoneyListener($player->getName());
+                if($instance->checkMoneyStorage()){
+                    $date1 = new DateTime($instance->getMoneyStorageDate());
+                    $date2 = new DateTime(date("Y/m/d"));
+                    if($date1 < $data2){
+                        $money = ConfigBase::getFor(ConfigList::CASH_STORAGE)->getNested($player->getName().".Money");
+                        ConfigBase::getFor(ConfigList::CASH_STORAGE)->setNested($player->getName().".Money",0);
+                        ConfigBase::getFor(ConfigList::CASH_STORAGE)->setNested($player->getName().".Date",0);
+                        ConfigBase::getFor(ConfigList::CASH_STORAGE)->save();
+                        GovernmentMoney::getInstance()->addMoney($money);
+                        $player->sendMessage("§a[個人通知] §7受取可能な保管金はありません");
+                    }else{
+                        $player->sendForm(new CashReceipt($player->getName()));
+                    }
+                }else{
+                    $player->sendMessage("§a[個人通知] §7受取可能な保管金はありません");
+                }
+            break;
 
             //政府の預金残高を照会（国庫)
-            case 1:
+            case 2:
                 $player->sendForm(new GovernmentDepositBalance());
             break;
 
             //地方財政状況照会
-            case 2:
+            case 3:
                 $player->sendForm(new LocalFinance());
             break;
 
             //権限者の確認
-            case 3:
+            case 4:
                 $player->sendForm(new GovernmentOfficial());
             break;
 
             //市町村別の財政を管理したり、政府関係者が市町村の管理をする
-            case 4:
+            case 5:
                 $player->sendForm(new AdministrativeManagement());
             break;
         }
@@ -56,6 +80,9 @@ class GovernmentMenu implements Form{
             'buttons'=>[
                 [
                     'text'=>'資源買取フォーム'
+                ],
+                [
+                    'text'=>'保管金受取'
                 ],
                 [
                     'text'=>'政府財政状況照会'
